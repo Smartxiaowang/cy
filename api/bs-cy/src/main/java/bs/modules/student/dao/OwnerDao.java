967085626a48f66cddcee36ac26464830225faa8
202122430302wangxuan <1261008090@qq.com>
2022-10-11 21:15:01 +0800

    init api

--- api/bs-cy/src/main/java/bs/modules/student/dao/OwnerDao.java
+++
@@ -1,9 +0,0 @@
-package bs.modules.student.dao;
-
-import bs.common.dao.BaseDao;
-import bs.modules.student.entity.OwnerEntity;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface OwnerDao extends BaseDao<OwnerEntity> {
-}
